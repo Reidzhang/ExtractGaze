1b7091c60691495926dd99e987d60cd14ff11f40
ZHITAO ZHANG <zzt124@ZHITAOs-MacBook-Pro.local>
2015-07-28 13:55:33 -0700

    change calculation

--- identify_pupildirection.py
+++ identify_pupildirection.py
@@ -10,45 +10,52 @@ import signal
 
 from sys import stdin, exit
 
+low_bar = 0.35
+hig_bar = 0.65
+
+org_x = 0.5
+org_y = 0.5
+inner_radius = 0.15
+out_radius = math.sqrt(0.5**2 + 0.5**2)
+
+
 def signal_handler(signal, frame):
 	print 'Exiting the program right now'
 	exit(0)
 
-def det_direction(confidence, norm_pos, con_level):
+def det_angle(confidence, norm_pos, con_level):
 	'''
-		Determite which direction the user
+		Determite which angle the user
 		is looking at
 	'''
 	# For x, y if the position is within [0.4, 0.7],
 	# eye would't be considered as looking at specific
-	# direction
+	# angle
 	if confidence > con_level:
 		# case: confidence is large enough for consideration
 		norm_x, norm_y = map(float, norm_pos[1:-1].split(','))
-		if norm_x > 0.7:
-			# case 1: eye is looking at rigth side
-			if norm_y < 0.4:
-				print 'SE'
-			elif norm_y > 0.7:
-				print 'NE'
-			else:
-				print 'E'
-		elif norm_x < 0.4:
-			# case 2: eye is looking at left side
-			if norm_y < 0.4:
-				print 'SW'
-			elif norm_y > 0.7:
-				print 'NW'
-			else:
-				print 'W'
+		diff_x = norm_x - org_x
+		diff_y = norm_y - org_y
+		if abs(diff_x) <= inner_radius 
+			and abs(diff_y) <= inner_radius:
+			pass
 		else:
-			# case 3: eye is looking at middle part
-			if norm_y < 0.4:
-				print 'S'
-			elif norm_y > 0.7:
-				print 'N'
+			# calculate hypotenuse
+			hypotenuse = math.sqrt(diff_x ** 2 + diff_y ** 2)
+			angle = 0.0
+			if (diff_x >= 0 and diff_y >= 0):
+				# first quadrant
+				angle = math.degrees(math.asin(diff_y / hypotenuse))
+			elif (diff_x < 0 and diff_y >= 0):
+				# second quadrant
+				angle = 180 - math.degrees(math.asin(diff_y / hypotenuse))
+			elif (diff_x < 0  and diff_y < 0):
+				# third quadrant
+				angle = 180 - math.degrees(math.asin(diff_y / hypotenuse))
 			else:
-				pass
+				# fourth quadrant
+				angle = 360 + math.degrees(math.asin(diff_y / hypotenuse))
+			print angle
 	else:
 		# confidence is too small to make a decision
 		pass
@@ -87,7 +94,7 @@ def main():
 				# norm_pos is a attribute in the dictionary as string
 				norm_pos = items['norm_pos']
 
-				det_direction(confidence, norm_pos, con_level)
+				det_angle(confidence, norm_pos, con_level)
 			except KeyError:
 				pass
 		else:
